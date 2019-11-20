fe530f2d20674ab5a70510d7f0c703aac713ef67
gitzky <927926261@qq.com>
2019-11-20 20:33:14 +0800

    update

--- src/views/User.vue
+++ src/views/User.vue
@@ -15,9 +15,11 @@
 
     <template>
       <el-table border :data="tableData" style="width: 100%">
-        <el-table-column prop="date" align="center" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" align="center" label="姓名" width="180"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column prop="created_at" align="center" label="日期"></el-table-column>
+        <el-table-column prop="user_name" align="center" label="姓名"></el-table-column>
+        <el-table-column prop="email" align="center" label="邮箱"></el-table-column>
+        <el-table-column prop="sex" align="center" label="性别"></el-table-column>
+        <el-table-column prop="headicon" align="center" label="地址"></el-table-column>
       </el-table>
     </template>
   </div>
@@ -27,28 +29,7 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      tableData: []
     };
   },
 
@@ -61,6 +42,9 @@ export default {
       console.log(this.$store);
       this.$store.dispatch("index/selUserList").then(res => {
         console.log(res);
+        if (res.code === "0") {
+          this.tableData = res.data.list || [];
+        }
       });
     }
   }
